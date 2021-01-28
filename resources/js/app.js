10fe0f77353dcb7a0e481f3903bbe89ac7eb1f28
Alex Contreras <ac@jexan.com>
2021-01-27 20:47:47 -0600

    Add header components as test of loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import React from 'react';
+import Header from './components/header/Header';
+
+class App extends React.Component {
+    render() {
+        return <Header />;
+    }
+}
+
+export default App;
