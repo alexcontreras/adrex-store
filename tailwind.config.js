45b5875de2f754d7e0cfd3c51968afb593340f11
Alex Contreras <ac@jexan.com>
2021-01-27 20:46:47 -0600

    Prepare UI scaffolding

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.jsx',
+    ],
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,24 @@
 const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/js/index.js', 'public/dist/js/app.js')
+    .react()
+    .postCss('resources/css/app.css', 'public/dist/css', [
+        require('tailwindcss'),
+    ])
+    .sourceMaps(false, 'source-map');
+
+mix.webpackConfig({
+    output: {
+        chunkFilename: 'dist/js/chunks/app.[name][contenthash].js',
+    },
+});
